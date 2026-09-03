4286c304a847bfc24a2a35df704f20e07e08dc6b
Abdullahi Oriola <abdullahioriola02@gmail.com>
2026-09-03 15:45:17 +0000

    feat: add Lga enum covering all 20 Lagos State LGAs

---
+++ src/main/java/com/group9/sdmfas/model/Lga.java
@@ -0,0 +1,28 @@
+package com.group9.sdmfas.model;
+
+/**
+ * The 20 Local Government Areas of Lagos State.
+ * Every incoming report gets routed to one of these.
+ */
+public enum Lga {
+    AGEGE,
+    AJEROMI_IFELODUN,
+    ALIMOSHO,
+    AMUWO_ODOFIN,
+    APAPA,
+    BADAGRY,
+    EPE,
+    ETI_OSA,
+    IBEJU_LEKKI,
+    IFAKO_IJAIYE,
+    IKEJA,
+    IKORODU,
+    KOSOFE,
+    LAGOS_ISLAND,
+    LAGOS_MAINLAND,
+    MUSHIN,
+    OJO,
+    OSHODI_ISOLO,
+    SHOMOLU,
+    SURULERE
+}
